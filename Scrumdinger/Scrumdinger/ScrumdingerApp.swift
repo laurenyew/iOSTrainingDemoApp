6fce4700fc2832ff522be92e1f314beaac754f1b
Lauren Yew <laurenyew@gmail.com>
2021-04-25 15:52:37 -0500

    Completed navigation view. Moving onto https://developer.apple.com/tutorials/app-dev-training/creating-the-edit-view

--- Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -13,7 +13,9 @@ struct ScrumdingerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: DailyScrum.data)
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.data)
+            }
         }
     }
 }
